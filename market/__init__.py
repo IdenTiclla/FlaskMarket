d71de4af7788aceed5befd6a5939d13575511c2f
unknown <iden.ticlla@gmail.com>
2021-08-05 19:07:09 -0400

    User Authentication part 2

--- market/__init__.py
+++ market/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
@@ -10,6 +10,9 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = '95cd72ac7f70b4ad5905be48'
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
 from market import routes
