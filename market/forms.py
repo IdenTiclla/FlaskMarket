e26b3314b9746c1ed84dd8fefeb7eea9c2d1db70
unknown <iden.ticlla@gmail.com>
2021-08-05 08:46:13 -0400

    Flask Validations

--- market/forms.py
+++ market/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 class RegisterForm(FlaskForm):
-    username = StringField(label='Username')
-    email_address = StringField(label='Email address')
-    password1 = PasswordField(label='Enter password')
-    password2 = PasswordField(label='Confirm password')
+    username = StringField(label='Username', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email address', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Enter password', validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(label='Confirm password', validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField(label='Create Account')
 
 
